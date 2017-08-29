6f5d4f0e164aeaf31663a873085681b238cf0330
shearwavesplitter <s.mroczek@outlook.com>
2017-08-29 18:27:47 +1200

    added no header option

--- R/fn_readMFAST.R
+++ R/fn_readMFAST.R
@@ -2,11 +2,13 @@
 #' @description Reads a .summ output from the original MFAST codes
 #' @param path The path the the summary file
 #' @param recuspid Regenerate unique cuspids from event names? (Useful if they have been truncated in MFAST)
+#' @param Does the summary file have a one line header?
 #' @return A dataframe containing the summary file
 #' @details This function is used with grade() to grade .summ files produced using the original MFAST codes (by setting mfast=TRUE).
 #' @export
-readmfast <- function(path,recuspid=FALSE){
-	tab <- read.csv(path,skip=1,header=F,stringsAsFactors=FALSE)
+readmfast <- function(path,recuspid=FALSE,header=TRUE){
+	if(header){skipper <- 1}else{skipper <- 0}
+	tab <- read.csv(path,skip=skipper,header=F,stringsAsFactors=FALSE)
 	colnames(tab) <-  c("event","stat","slat","slon","cuspid","year","doy_det","evla","evlo","distevstat","depthkm","mag","baz","spol","Dspol","wbeg","wend","dist_ruap_km","dist_ruap_deg","SNR","tlag","Dtlag","fast","Dfast","anginc","anginc_corr","type_ini","timestamp","comment","nyquist","gradeABCNR","filt_lo","filt_HI","spolfast","bandang","pickgrade","lambdamax","ndf","lambda_min","ttime","maxfreq")
 	null <- rep(FALSE,length(tab$fast))
 	for(i in 1:length(tab$fast)){
